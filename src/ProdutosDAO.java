d5cf3155f70a5d913a61e40ff6fa2fe715d0b66b
FerCarinheno <fernandocarinheno@hotmail.com>
2024-08-16 09:23:48 -0300

    Botão salvar cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -25,12 +25,25 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
          
-        //conn = new conectaDAO().connectDB();
-        
-        
+        conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO PRODUTOS (nome, valor, status) VALUES (?,?,?)";
+        try {
+            PreparedStatement prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setDouble(2,produto.getValor());
+            prep.setString(3,produto.getStatus());
+            prep.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null,"Produto cadastrado com sucesso");
+            
+        }catch (Exception e){
+            System.out.println("Erro ao cadastrar" + e.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
+        
+        
         
         return listagem;
     }
